cd7a645340d49bae1063aa8ee2fa1aea1ea43b30
sleeter <lfmonsterfeed@gmail.com>
2024-11-05 01:25:22 +0300

    comment tests

--- backend/infosys/src/test/kotlin/itmo/sleeter/infosys/InfosysApplicationTests.kt
+++ backend/infosys/src/test/kotlin/itmo/sleeter/infosys/InfosysApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class InfosysApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
