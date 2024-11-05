746482f3c6e058eaea2c4888739e3385bc9ffece
sleeter <lfmonsterfeed@gmail.com>
2024-11-06 02:47:11 +0300

    add methods to flat service

--- backend/infosys/src/main/kotlin/itmo/sleeter/infosys/service/FlatService.kt
+++ backend/infosys/src/main/kotlin/itmo/sleeter/infosys/service/FlatService.kt
@@ -2,6 +2,12 @@ package itmo.sleeter.infosys.service
 
 import itmo.sleeter.infosys.dto.request.CreateFlatRequest
 import itmo.sleeter.infosys.dto.response.FlatResponse
+import itmo.sleeter.infosys.dto.response.FurnishResponse
+import itmo.sleeter.infosys.dto.response.TransportResponse
+import itmo.sleeter.infosys.dto.response.ViewResponse
+import itmo.sleeter.infosys.enumeration.Furnish
+import itmo.sleeter.infosys.enumeration.Transport
+import itmo.sleeter.infosys.enumeration.View
 import itmo.sleeter.infosys.exception.FlatNotFoundException
 import itmo.sleeter.infosys.mapper.FlatMapper
 import itmo.sleeter.infosys.repository.FlatRepository
@@ -18,7 +24,7 @@ class FlatService(
 
     fun getFlatById(id : Long) : FlatResponse {
         val flat = flatRepository
-            .findFlatByIdWithCoordinatesAndHouseAndUserCreateAndUserUpdateFetch(id)
+            .findFlatById(id)
             .orElseThrow {
                 FlatNotFoundException("Flat with id=$id not found")
             }
@@ -29,6 +35,15 @@ class FlatService(
                 userService.userToUserResponse(flat.userCreate),
                 userService.userToUserResponse(flat.userUpdate)
             )
+    }
+    fun getFlats() : List<FlatResponse> = flatRepository.findAll().map {
+        flat -> flatMapper.flatToFlatResponse(
+            flat,
+            coordinateService.coordinateToCoordinateResponse(flat.coordinates),
+            houseService.houseToHouseResponse(flat.house),
+            userService.userToUserResponse(flat.userCreate),
+            userService.userToUserResponse(flat.userUpdate)
+        )
     }
     fun deleteAllByHouseId(houseId: Long) {
         flatRepository.deleteAllByHouseId(houseId)
@@ -53,6 +68,12 @@ class FlatService(
             userService.userToUserResponse(user)
         )
     }
+    fun deleteFlatById(id: Long) {
+        flatRepository.deleteById(id)
+    }
+    fun getFurnish() : FurnishResponse = FurnishResponse(Furnish.entries.map { f -> f.name })
+    fun getTransport() : TransportResponse = TransportResponse(Transport.entries.map { t -> t.name })
+    fun getView() : ViewResponse = ViewResponse(View.entries.map { v -> v.name })
 }
 
 
