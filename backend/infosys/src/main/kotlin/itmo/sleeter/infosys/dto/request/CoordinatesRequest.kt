108a34a73dee68a3324db276586f91d814bc4730
sleeter <lfmonsterfeed@gmail.com>
2024-11-06 02:44:57 +0300

    add validation

--- backend/infosys/src/main/kotlin/itmo/sleeter/infosys/dto/request/CoordinatesRequest.kt
+++ backend/infosys/src/main/kotlin/itmo/sleeter/infosys/dto/request/CoordinatesRequest.kt
@@ -1,6 +1,11 @@
 package itmo.sleeter.infosys.dto.request
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+
 data class CoordinatesRequest(
+    @Max(598)
     val x : Long,
+    @Min(-254)
     val y : Double
 )
--- backend/infosys/src/main/kotlin/itmo/sleeter/infosys/dto/request/CreateHouseRequest.kt
+++ backend/infosys/src/main/kotlin/itmo/sleeter/infosys/dto/request/CreateHouseRequest.kt
@@ -1,12 +1,19 @@
 package itmo.sleeter.infosys.dto.request
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import org.jetbrains.annotations.NotNull
 
 data class CreateHouseRequest(
+    @NotNull
     val name: String,
     @JsonProperty("number_of_lifts")
+    @Min(1)
     val numberOfLifts: Long,
+    @Min(1) @Max(901)
     val year: Int,
     @JsonProperty("user_id")
+    @Min(0)
     val userId: Long
 )
