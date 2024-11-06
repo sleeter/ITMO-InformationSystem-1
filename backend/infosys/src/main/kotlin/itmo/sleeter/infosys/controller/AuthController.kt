533b8a71e493ba93e4ea48f492452e6dc41b463a
sleeter <lfmonsterfeed@gmail.com>
2024-11-06 03:03:50 +0300

    handlers for login and register

--- backend/infosys/src/main/kotlin/itmo/sleeter/infosys/controller/AuthController.kt
+++ backend/infosys/src/main/kotlin/itmo/sleeter/infosys/controller/AuthController.kt
@@ -1,7 +1,13 @@
 package itmo.sleeter.infosys.controller
 
+import itmo.sleeter.infosys.dto.request.LoginRequest
+import itmo.sleeter.infosys.dto.request.RegisterRequest
+import itmo.sleeter.infosys.dto.response.TokenResponse
 import itmo.sleeter.infosys.service.UserService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(private val userService: UserService) {
 
     @PostMapping("/login")
-    fun login() {
-        return
+    fun login(@RequestBody @Valid req: LoginRequest): ResponseEntity<TokenResponse> {
+        val token = userService.login(req)
+        return ResponseEntity.ok(token)
     }
 
     @PostMapping("/register")
-    fun register() {
-        return
+    fun register(@RequestBody @Valid req: RegisterRequest): ResponseEntity<TokenResponse> {
+        val token = userService.register(req)
+        return ResponseEntity.ok(token)
     }
 }
