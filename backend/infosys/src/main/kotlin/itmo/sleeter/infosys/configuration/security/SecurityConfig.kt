596cc6276498808287c08c76924f90feb25203ec
sleeter <lfmonsterfeed@gmail.com>
2024-12-17 22:40:13 +0300

    fix cors

--- backend/infosys/src/main/kotlin/itmo/sleeter/infosys/configuration/security/SecurityConfig.kt
+++ backend/infosys/src/main/kotlin/itmo/sleeter/infosys/configuration/security/SecurityConfig.kt
@@ -22,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -59,7 +60,17 @@ class SecurityConfig(
         }
         return http
             .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
-            .cors { obj: CorsConfigurer<HttpSecurity> -> obj.disable() }
+            .cors { cors ->
+                cors.configurationSource { request ->
+                    val corsConfiguration = CorsConfiguration()
+                    corsConfiguration.allowedOriginPatterns = listOf("*")
+                    corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    corsConfiguration.allowedHeaders = listOf("*")
+                    corsConfiguration.allowCredentials = true
+                    corsConfiguration
+                }
+            }
+//            .cors { obj: CorsConfigurer<HttpSecurity> -> obj.disable() }
             .sessionManagement { obj: SessionManagementConfigurer<HttpSecurity> -> obj.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
